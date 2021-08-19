f65c77b2ef085e38188ab32b23621a8de2fe02b5
benjamin bowie <bbowie@unfi.com>
2021-08-19 16:51:17 +0000

    update s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
+module "s3-bucket" {
+  source  = "app.terraform.io/bbowie-training/s3-bucket/aws"
+  version = "2.7.0"
   bucket_prefix = "bbowie"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
 }
