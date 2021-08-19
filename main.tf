1125569b9dfae10f704819058720615a80612d77
benjamin bowie <bbowie@unfi.com>
2021-08-19 16:34:02 +0000

    Added Billable tag

--- main.tf
+++ main.tf
@@ -133,7 +133,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    department = "devops"
+    Department = "devops"
+    Billable = true
   }
 }
 
